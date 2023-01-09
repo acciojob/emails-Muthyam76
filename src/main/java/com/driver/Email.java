3fb4479409f8256fa67c50d4a57b39fc84228596
muthyam <dadigemuthyam@gmail.com>
2023-01-09 21:29:50 +0530

    first

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,26 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword.equals(newPassword) && newPassword.length()>=8){
+            int upperCase=0;
+            int lowerCase=0;
+            int digit=0;
+            int special=0;
+            for(int i=0;i<newPassword.length();i++){
+                char c=newPassword.charAt(i);
+                if(c>=65 && c<=90)
+                    upperCase++;
+                else if(c>=97 && c<=122)
+                    lowerCase++;
+                else if(c>='0' && c<='9')
+                    digit++;
+                else
+                    special++;
+            }
+            if(upperCase>0 && lowerCase>0 && digit>0 && special>0){
+                password=newPassword;
+            }
+
+        }
     }
 }
